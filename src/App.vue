9664e00def32a2c7184d6eea4d490ec2772cba11
sunyizhe1 <syz18058049008@163.com>
2022-06-22 18:02:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  created () {},
+  data () {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  filters: {},
+  components: {}
+}
+</script>
+
+<style scoped>
+
+</style>
